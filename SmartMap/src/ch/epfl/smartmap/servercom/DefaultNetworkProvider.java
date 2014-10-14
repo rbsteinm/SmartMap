331ecab88d4e4f52e8b392926a89d1da8799c145
Robin Genolet <robin.genolet@epfl.ch>
2014-10-15 00:14:51 +0200

    Add the package servercom (inspired from HW2)

---
+++ SmartMap/src/ch/epfl/smartmap/servercom/DefaultNetworkProvider.java
@@ -0,0 +1,32 @@
+package ch.epfl.smartmap.servercom;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A default implementation of the {@link NetworkProvider} interface that uses
+ * the mechanism available in the {@link URL} object to create
+ * {@link HttpURLConnection} objects.
+ *
+ * @author SpicyCH
+ */
+public class DefaultNetworkProvider implements NetworkProvider {
+
+    /**
+     * The default constructor.
+     */
+    public DefaultNetworkProvider() {
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see ch.epfl.sweng.quizapp.NetworkProvider#getConnection(java.net.URL)
+     */
+    @Override
+    public HttpURLConnection getConnection(URL url) throws IOException {
+        return (HttpURLConnection) url.openConnection();
+    }
+
+}
