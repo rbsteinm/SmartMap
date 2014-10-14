f2b39ce1c5a6d2d270bb501e26529c57675b6aef
Robin Genolet <robin.genolet@epfl.ch>
2014-10-14 21:05:17 +0200

    Code for the authentication until I got stuck needing the implementations of other group members

--- SmartMap/src/ch/epfl/smartmap/authentication/package-info.java
+++ SmartMap/src/ch/epfl/smartmap/authentication/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This package contains all classes used at the client-side for authentication
+ * Time spent:
+ *  Thinking and discussing with the team about the method used to authenticate, developing the base: 3h
  * 
  * @author SpicyCH
  */
