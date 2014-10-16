4d7398bd01d63df948e824227727dea57f70465b
jfperren <julien.perrenoud@epfl.ch>
2014-10-16 11:05:55 +0200

    Add attrs.xml, click functionality on BottomSlider

--- SmartMap/src/ch/epfl/smartmap/gui/BottomSlider.java
+++ SmartMap/src/ch/epfl/smartmap/gui/BottomSlider.java
@@ -1,63 +1,112 @@
-/**
- * 
- */
 package ch.epfl.smartmap.gui;
 
 import ch.epfl.smartmap.R;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
+ *
  * @author jfperren
  *
  */
-public class BottomSlider extends LinearLayout {
+public class BottomSlider extends RelativeLayout {
+    /**
+     * Represents the visual state of the Slider
+     * 
+     * @author jfperren
+     */
 
     public final static int SPEED = 2;
-    public enum State {HIDDEN, DISCRETE, EXTENDED};
-    
-    private State mState;
+    // Same values as in xml file
+    public final static int HIDDEN = 0;
+    public final static int DISCRETE = 1;
+    public final static int EXTENDED = 2;
+    private int mState;
     /**
      * @param context
      */
-    public BottomSlider(Context context) {
-        super(context);
+    public BottomSlider(Context context, AttributeSet attrs) {
+        super(context, attrs);
+       
+        // Try to get the default value of the attributes
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+            attrs,
+            R.styleable.BottomSlider,
+            0, 0);
         
-        mState = State.HIDDEN;
-        this.setVisibility(View.GONE);
+        try {
+            mState = a.getInteger(R.styleable.BottomSlider_state, 0);
+        } finally {
+            a.recycle();
+        }
+        
+        // Set visibility attribute according to state
+        if (mState == HIDDEN) {
+            this.setVisibility(View.GONE);
+        } else {
+            this.setVisibility(View.VISIBLE);
+        }
     }
-
-    public void setState(State newState){
-       assert newState != mState : "setState : Should set to a different state";
+    
+    public int getState() {
+        return mState;
+    }
+    
+    public void setState(int newState) {
+        assert newState != mState : "setState : Should set to a different state";
        
-       Animation animation = null;
+        switch(newState){
+            case HIDDEN:
+                this.setVisibility(View.GONE);
+                this.getLayoutParams().height = 0;
+                break;
+            case DISCRETE:
+                this.setVisibility(View.VISIBLE);
+                this.getLayoutParams().height = LayoutParams.WRAP_CONTENT;
+                break;
+            case EXTENDED: 
+                this.setVisibility(View.VISIBLE);
+                this.getLayoutParams().height = LayoutParams.MATCH_PARENT;
+                break;
+            default:
+                assert false;
+        }
        
-       switch(newState){
-           case HIDDEN:
-               this.setVisibility(View.VISIBLE);
-               animation = AnimationUtils.loadAnimation(this.getContext(), R.animator.bottom_slider_hidden_to_discrete);
-               this.startAnimation(animation);
-               
-               break;
-           case DISCRETE:
-               break;
-           case EXTENDED:
-               break;
-           default:
-               assert false;
-               
-               
-               
-           assert (this.getVisibility() == View.GONE && mState == State.HIDDEN)
-               || (this.getVisibility() == View.VISIBLE && (mState == State.DISCRETE || mState == State.EXTENDED))
-               : "setState : Inconsistency between Visibility and state attributes.";
-               
-       }
-       
-       animation.setDuration(SPEED);
-       
+        mState = newState;
+        
+        assert (this.getVisibility() == View.GONE && mState == HIDDEN)
+            || (this.getVisibility() == View.VISIBLE && (mState == DISCRETE || mState == EXTENDED))
+            : "setState : Inconsistency between Visibility and state attributes.";
+           
+        
+        
+        invalidate();
+        requestLayout();
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            if (this.getState() != BottomSlider.DISCRETE) {
+                Log.d("Touchevent", "setState discrete");
+                this.setState(BottomSlider.DISCRETE);
+            } else {
+                this.setState(BottomSlider.EXTENDED);
+            }
+        }
+        
+        return true;
     }
 }
--- SmartMap/src/ch/epfl/smartmap/gui/MainActivity.java
+++ SmartMap/src/ch/epfl/smartmap/gui/MainActivity.java
@@ -2,26 +2,44 @@ package ch.epfl.smartmap.gui;
 
 import ch.epfl.smartmap.R;
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+/**
+ * @author jfperren
+ *
+ */
 public class MainActivity extends Activity {
 
+    private float startY;
+    private BottomSlider mBottomSlider;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                .add(R.id.container, new PlaceholderFragment()).commit();
-        }
+        
+        mBottomSlider = (BottomSlider) findViewById(R.id.bottom_slider);
+        mBottomSlider.setVisibility(View.VISIBLE);
+        
+        mBottomSlider.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (mBottomSlider.getState() != BottomSlider.DISCRETE) {
+                    mBottomSlider.setState(BottomSlider.DISCRETE);
+                } else {
+                    mBottomSlider.setState(BottomSlider.HIDDEN);
+                }
+            }
+        });
     }
 
     @Override
@@ -42,21 +60,45 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+    
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN : 
+//                startY = event.getY();
+//                break;           
+//            
+//            case MotionEvent.ACTION_UP: 
+//                float endY = event.getY();
+//                 
+//                if (endY < startY) {
+//                    Log.d("BottomSlider", "moving up");
+//                    mBottomSlider.setState(State.DISCRETE);
+//                } else {
+//                    Log.d("BottomSlider", "moving down");
+//                    mBottomSlider.setState(State.HIDDEN);
+//                }
+//                break;
+//            default:
+//                assert false;
+//        }
+//        
+//        return true;
+//    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            if (mBottomSlider.getState() == BottomSlider.HIDDEN) {
+                Log.d("Touchevent", "setState discrete");
+                TextView tv = (TextView) findViewById(R.id.textview);
+                tv.setText("Test");
+                mBottomSlider.setState(BottomSlider.DISCRETE);
+            } else {
+                mBottomSlider.setState(BottomSlider.HIDDEN);
+            }
         }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_map, container,
-                false);
-            return rootView;
-        }
+        
+        return true;
     }
 }
