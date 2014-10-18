1af4d2ce3b659b3eb8702bdb53d4b5479cd4497f
jfperren <julien.perrenoud@epfl.ch>
2014-10-18 16:45:05 +0200

    Add basic search bar, background mock image, and search bar related panel sliding

--- SmartMap/src/ch/epfl/smartmap/gui/MainActivity.java
+++ SmartMap/src/ch/epfl/smartmap/gui/MainActivity.java
@@ -1,10 +1,20 @@
 package ch.epfl.smartmap.gui;
 
 
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
 import ch.epfl.smartmap.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.view.KeyEvent;
 
 /**
  * @author jfperren
@@ -12,11 +22,39 @@ import android.view.Menu;
  */
 public class MainActivity extends Activity {
     
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        final EditText mSearchBarEditText = (EditText) findViewById(R.id.searchBarEditText);
+        final SlidingUpPanelLayout mBottomSlider = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        
+        mSearchBarEditText.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Log.d("searchBar", "Search Request Sent");
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+        
+        mSearchBarEditText.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    mBottomSlider.expandPanel();
+                    Log.d("SearchBar", "Got focus");
+                } else {
+                    mBottomSlider.hidePanel();
+                    Log.d("SearchBar", "Lost focus");
+                }
+            }
+        });
     }
 
     @Override
@@ -25,4 +63,17 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+
+    @Override
+    public void onBackPressed() {
+        final SlidingUpPanelLayout mBottomSlider = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+
+        if (mBottomSlider != null && mBottomSlider.isPanelExpanded() || mBottomSlider.isPanelAnchored()) {
+            mBottomSlider.collapsePanel();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
