c95b464176c7b1404ee498c3d53d382edca35f7b
Robin Genolet <robin.genolet@epfl.ch>
2014-10-27 21:10:48 +0100

    Modify tests

--- server/tests/AuthenticationControllerTest.php
+++ server/tests/AuthenticationControllerTest.php
@@ -27,12 +27,15 @@ use Doctrine\DBAL\Configuration;
 
 
 /**
- * 
+ *
  * To run theses tests, you need to
- *  -Have a db named smartmap live on localhost, that has the user root with no pwd
- *  -Set these settings in web/index.php
- *  -Set the bootstrap to autoload.php : php phpunit.phar --bootstrap ..\..\vendor\autoload.php [TESTS TO RUN]
- * 
+ * -Have a db named smartmap live on localhost, that has the user root with no pwd
+ * -Set these settings in web/index.php
+ * -Set the bootstrap to autoload.php : php phpunit.phar --bootstrap ..\..\vendor\autoload.php [TESTS TO RUN]
+ *
+ * In the case that some tests fail in the future, try connecting to SmartMap app with the mock user. It might need to
+ * reaccept the permissions.
+ *
  * The mock fb account is robin.genolet@epfl.ch hackerinside
  * name: Swag Sweng
  * ID: 1482245642055847
@@ -47,6 +50,7 @@ class AuthenticationControllerTest extends PHPUnit_Framework_TestCase {
     
     private $authController;
     private $validPostRequest;
+    private $validFbToken;
     
     public function setUp() {
         $config = new Configuration ();
@@ -61,20 +65,22 @@ class AuthenticationControllerTest extends PHPUnit_Framework_TestCase {
         $conn = DriverManager::getConnection ( $connectionParams, $config );
         
         $userRepo = new UserRepository ( $conn );
-        $authContr = new AuthenticationController ( $userRepo );
+        $authContr = new AuthenticationController ( $userRepo, '305881779616905', 'b851a1eb3edcaf637f92fbb2af2b3b47' );
         
         $this->authController = $authContr;
+        
+        $this->validFbToken = "CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7" .
+                        "FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQ" .
+                        "NWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy";
         
         $this->validPostRequest = array (
                         "name" => "SmartMap SwEng",
                         "facebookId" => "1482245642055847",
-                        "facebookToken" => "CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7" .
-                        "FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQ" .
-                        "NWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy" 
+                        "facebookToken" => $this->validFbToken
         );
     }
     
-    
+
     public function testCanLoginWithGoodRequestParams() {
         $request = new Request ( $getRequest = array (), $this->validPostRequest );
         
@@ -88,17 +94,23 @@ class AuthenticationControllerTest extends PHPUnit_Framework_TestCase {
     
     }
     
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage Session is null.
+     */
     public function testNoSessionYieldsException() {
-        $this->setExpectedException ( 'SmartMap\Control\ControlException' );
         $request = new Request ( $getRequest = array (), $this->validPostRequest );
         $serverResponse = $this->authController->authenticate ( $request );
     }
     
-    public function testCannotLoginWithBadRequestParams() {
-        $this->setExpectedException ( 'SmartMap\Control\ControlException' );
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage Bad fb session
+     */
+    public function testCannotLoginWithBadSession() {
         $postReq = array (
-                        "name" => "Robich",
-                        "facebookId" => "123",
+                        "name" => "SmartMap SwEng",
+                        "facebookId" => "1482245642055847",
                         "facebookToken" => "ehf h e ue" 
         );
         $request = new Request ( $getRequest = array (), $postReq );
@@ -112,20 +124,106 @@ class AuthenticationControllerTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals ( "error", $json->status );
     }
     
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage do not match the fb access token
+     */
+    public function testCannotLoginWithGoodSessionButBadName() {
+        $postReq = array (
+                        "name" => "Robich",
+                        "facebookId" => "1482245642055847",
+                        "facebookToken" => $this->validFbToken 
+        );
+        $request = new Request ( $getRequest = array (), $postReq );
+        
+        $session = new Session ( new MockFileSessionStorage () );
+        $request->setSession ( $session );
+        
+        $serverResponse = $this->authController->authenticate ( $request );
+        $json = json_decode ( $serverResponse->getContent () );
+        
+        $this->assertEquals ( "error", $json->status );
+    }
+    
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage do not match the fb access token
+     */
+    public function testCannotLoginWithGoodSessionButBadId() {
+        $postReq = array (
+                        "name" => "SmartMap SwEng",
+                        "facebookId" => "1337",
+                        "facebookToken" => $this->validFbToken 
+        );
+        $request = new Request ( $getRequest = array (), $postReq );
+        
+        $session = new Session ( new MockFileSessionStorage () );
+        $request->setSession ( $session );
+        
+        $serverResponse = $this->authController->authenticate ( $request );
+        $json = json_decode ( $serverResponse->getContent () );
+        
+        $this->assertEquals ( "error", $json->status );
+    }
+    
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage An error occured while dealing with the database
+     */
+    public function testDatabaseErrorYieldsRightExceptionMessage() {
+        $config = new Configuration ();
+        $connectionParams = array (
+                        'dbname' => 'smartmap',
+                        'user' => 'Robich',
+                        'password' => 'LetMeIn',
+                        'host' => 'localhost',
+                        'driver' => 'pdo_mysql' 
+        );
+        
+        $conn = DriverManager::getConnection ( $connectionParams, $config );
+        
+        $userRepo = new UserRepository ( $conn );
+        $authContr = new AuthenticationController ( $userRepo, '305881779616905', 'b851a1eb3edcaf637f92fbb2af2b3b47' );
+        
+        $request = new Request ( $getRequest = array (), $this->validPostRequest );
+        
+        $session = new Session ( new MockFileSessionStorage () );
+        $request->setSession ( $session );
+        
+        $serverResponse = $authContr->authenticate ( $request );
+        $json = json_decode ( $serverResponse->getContent () );
+        
+        $this->assertEquals ( "Some text, shouldn't matter!", $json->status );
+    }
+    
+    /**
+     * @expectedException SmartMap\Control\ControlException
+     * @expectedExceptionMessage Missing POST parameter
+     */
+    public function testNoPostParamsYieldException() {
+        $request = new Request ( $getRequest = array (), $postParams = array () );
+        
+        $session = new Session ( new MockFileSessionStorage () );
+        $request->setSession ( $session );
+        
+        $serverResponse = $this->authController->authenticate ( $request );
+        $json = json_decode ( $serverResponse->getContent () );
+        
+        $this->assertEquals ( "OK", $json->status );
+    }
+    
     public function testSessionUserIdIsSetAfterLogin() {
         $request = new Request ( $getRequest = array (), $this->validPostRequest );
         
         $session = new Session ( new MockFileSessionStorage () );
         $request->setSession ( $session );
         
-        $this->assertTrue($session->get('userId') == null);
+        $this->assertTrue ( $session->get ( 'userId' ) == null );
         
         $serverResponse = $this->authController->authenticate ( $request );
         
-        $this->assertTrue($session->get('userId') > 0);
+        $this->assertTrue ( $session->get ( 'userId' ) > 0 );
     }
 
 }
-
-
 ?>
