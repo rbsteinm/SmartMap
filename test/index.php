e271b5630646431b411c33b0efa7e2cbc40a4b2a
Matthieu Girod <matthieu.girod@epfl.ch>
2014-10-27 15:32:34 +0100

    Deleted SlidingUpPanel library and SmartMapTestTest project

--- test/index.php
+++
@@ -1,6 +0,0 @@
-<form action="http://localhost/server-alias/web/index.php/auth" method="POST" />
-	<label for="name">Name: </label><input name="name"  value="SmartMap SwEng" /><br />
-	<label for="facebookToken">facebook token: </label><input name="facebookToken" value="CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQNWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy" size="400" /><br />
-	<label for="facebookId">facebook ID: </label><input name="facebookId" value="1482245642055847" /><br />
-	<input type="submit" value="submit" />
-</form>
