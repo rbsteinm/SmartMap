7aa3df260c9d303f8f40a245bb2f91f71a9298d4
Robin Genolet <robin.genolet@epfl.ch>
2014-10-28 18:39:49 +0100

    Modify marion's tests to match new method signature (authServer

--- SmartMapTest/src/ch/epfl/smartmap/test/severcom/NetworkAuthenticationClientTest.java
+++ SmartMapTest/src/ch/epfl/smartmap/test/severcom/NetworkAuthenticationClientTest.java
@@ -8,23 +8,22 @@ import ch.epfl.smartmap.servercom.NetworkAuthenticationClient;
 import ch.epfl.smartmap.servercom.SmartMapClientException;
 
 public class NetworkAuthenticationClientTest extends AndroidTestCase {
-	private static String SERVER_URL = "http://smartmap.ddns.net";
-	private final long facebookId = 1482245642055847L;
-	private final String name = "SmartMap SwEng";
-	private final String fbAccessToken = "CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQNWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy";
+    private static String SERVER_URL = "http://smartmap.ddns.net";
+    private final long facebookId = 1482245642055847L;
+    private final String name = "SmartMap SwEng";
+    private final String fbAccessToken = "CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQNWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy";
 
-	public NetworkAuthenticationClientTest() {
-		super();
-	}
+    public NetworkAuthenticationClientTest() {
+        super();
+    }
 
-	@Test
-	public void test() throws SmartMapClientException {
-		DefaultNetworkProvider provider = new DefaultNetworkProvider();
-		NetworkAuthenticationClient networkAuthClient = new NetworkAuthenticationClient(
-				SERVER_URL, provider);
+    @Test
+    public void test() throws SmartMapClientException {
+        DefaultNetworkProvider provider = new DefaultNetworkProvider();
+        NetworkAuthenticationClient networkAuthClient = new NetworkAuthenticationClient(SERVER_URL, provider);
 
-		networkAuthClient.authServer(name, facebookId, fbAccessToken);
+        networkAuthClient.authServer(name, facebookId, fbAccessToken);
 
-	}
+    }
 
 }
