2dcb0b0d2c5ab03a373c7ce4225d0b2b9ff2ef73
Hugo Sbai <hugo.sbai@epfl.ch>
2014-11-06 09:22:06 +0100

    Add some functionalities to FriendMarkerDisplayer, to associate each marker with the friend it represents

--- SmartMap/src/ch/epfl/smartmap/gui/FriendMarkerDisplayer.java
+++ SmartMap/src/ch/epfl/smartmap/gui/FriendMarkerDisplayer.java
@@ -3,12 +3,15 @@
  */
 package ch.epfl.smartmap.gui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import ch.epfl.smartmap.cache.Friend;
+import ch.epfl.smartmap.cache.User;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
 
 /**
  * An interface that provides a method to set friend's markers on the map
@@ -18,15 +21,39 @@ import com.google.android.gms.maps.GoogleMap;
  */
 public interface FriendMarkerDisplayer {
 
-    /**
-     * Set markers on the map for the friends in the list listOfFriends
-     * 
-     * @param context
-     * @param mGoogleMap
-     *            the map where we want to add markers
-     * @param listOfFriends
-     *            the friends we want to display
-     */
-    void setMarkersToMaps(Context context, GoogleMap mGoogleMap,
-        List<Friend> listOfFriends);
+	/**
+	 * Set markers on the map for the friends in the list listOfFriends
+	 * 
+	 * @param context
+	 * @param mGoogleMap
+	 *            the map where we want to add markers
+	 * @param listOfFriends
+	 *            the friends we want to display
+	 */
+	void setMarkersToMaps(Context context, GoogleMap mGoogleMap,
+			List<Friend> listOfFriends);
+
+	/**
+	 * 
+	 * @param friend
+	 * @return true if the friend is displayed
+	 */
+	public boolean isDisplayedFriend(User friend);
+
+	/**
+	 * @param friend
+	 * @return true if the marker is displayed
+	 */
+	public boolean isDisplayedMarker(User friend);
+
+	/**
+	 * @return the list of the markers that are displayed
+	 */
+	public List<Marker> getDisplayedMarkers();
+
+	/**
+	 * @param marker
+	 * @return the friend that the marker represents
+	 */
+	public User getFriendForMarker(Marker marker);
 }
--- SmartMap/src/ch/epfl/smartmap/gui/ProfilePictureFriendMarkerDisplayer.java
+++ SmartMap/src/ch/epfl/smartmap/gui/ProfilePictureFriendMarkerDisplayer.java
@@ -3,14 +3,21 @@
  */
 package ch.epfl.smartmap.gui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import ch.epfl.smartmap.cache.Friend;
+import ch.epfl.smartmap.cache.User;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -20,31 +27,58 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * 
  */
 public class ProfilePictureFriendMarkerDisplayer implements
-    FriendMarkerDisplayer {
+		FriendMarkerDisplayer {
 
-    public static final float MARKER_ANCHOR_X = (float) 0.5;
-    public static final float MARKER_ANCHOR_Y = 1;
+	public static final float MARKER_ANCHOR_X = (float) 0.5;
+	public static final float MARKER_ANCHOR_Y = 1;
 
-    public static final int WIDTH = 50;
-    public static final int HEIGHT = 50;
+	public static final int WIDTH = 50;
+	public static final int HEIGHT = 50;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ch.epfl.smartmap.gui.FriendMarker#setMarkersToMaps(android.content.Context ,
-     * com.google.android.gms.maps.GoogleMap, java.util.List)
-     */
-    public void setMarkersToMaps(Context context, GoogleMap mGoogleMap,
-        List<Friend> listOfFriends) {
+	/**
+	 * A map that contains the displayed markers, associated with the friend
+	 * they represent
+	 */
+	private Map<Marker, User> displayedMarkers = new HashMap<Marker, User>();
 
-        // Add marker with profile picture for each friend
-        for (Friend friend : listOfFriends) {
-            Bitmap friendProfilePicture = Bitmap.createScaledBitmap(
-                friend.getPicture(context), WIDTH, HEIGHT, false);
-            mGoogleMap.addMarker(new MarkerOptions()
-                .position(friend.getLatLng()).title(friend.getName())
-                .icon(BitmapDescriptorFactory.fromBitmap(friendProfilePicture))
-                .anchor((float) MARKER_ANCHOR_X, MARKER_ANCHOR_Y));
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ch.epfl.smartmap.gui.FriendMarker#setMarkersToMaps(android.content.Context
+	 * , com.google.android.gms.maps.GoogleMap, java.util.List)
+	 */
+	public void setMarkersToMaps(Context context, GoogleMap googleMap,
+			List<Friend> listOfFriends) {
+
+		// Add marker with profile picture for each friend
+		for (Friend friend : listOfFriends) {
+			Bitmap friendProfilePicture = Bitmap.createScaledBitmap(
+					friend.getPicture(context), WIDTH, HEIGHT, false);
+			Marker marker = googleMap.addMarker(new MarkerOptions()
+					.position(friend.getLatLng())
+					.title(friend.getName())
+					.icon(BitmapDescriptorFactory
+							.fromBitmap(friendProfilePicture))
+					.anchor((float) MARKER_ANCHOR_X, MARKER_ANCHOR_Y));
+
+			displayedMarkers.put(marker, friend);
+		}
+	}
+
+	public boolean isDisplayedFriend(User friend) {
+		return displayedMarkers.containsValue(friend);
+	}
+
+	public boolean isDisplayedMarker(User friend) {
+		return displayedMarkers.containsValue(friend);
+	}
+
+	public List<Marker> getDisplayedMarkers() {
+		return new ArrayList<Marker>(displayedMarkers.keySet());
+	}
+
+	public User getFriendForMarker(Marker marker) {
+		return displayedMarkers.get(marker);
+	}
 }
