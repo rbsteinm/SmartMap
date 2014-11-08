9a95c018b5d090b5a95aa8f30faabcfba44ca276
Alain Milliet <alain.milliet@epfl.ch>
2014-11-08 14:03:57 +0100

    just added 1 method in startActivityTest

--- SmartMapTest/src/ch/epfl/smartmap/test/gui/StartActivityTest.java
+++ SmartMapTest/src/ch/epfl/smartmap/test/gui/StartActivityTest.java
@@ -27,5 +27,10 @@ public class StartActivityTest extends
 		onView(withId(R.id.logo)).perform(click());
 		onView(withId(R.id.logo)).check(matches(isDisplayed()));
 	}
+	
+	public void testWelcomeClick() throws Exception {
+		onView(withId(R.id.welcome)).perform(click());
+		onView(withId(R.id.welcome)).check(matches(isDisplayed()));
+	}
 
 }
