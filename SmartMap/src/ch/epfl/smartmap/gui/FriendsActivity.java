7968e8ec9a66ec6fa1d2a58306e76114865ed15a
rbsteinm <raphael.steinmann@epfl.ch>
2014-11-10 21:26:05 +0100

    refactoring changes

--- SmartMap/src/ch/epfl/smartmap/gui/FriendsActivity.java
+++ SmartMap/src/ch/epfl/smartmap/gui/FriendsActivity.java
@@ -19,31 +19,33 @@ import ch.epfl.smartmap.cache.User;
  */
 public class FriendsActivity extends ListActivity {
     // Mock stuff
-    private List<User> mockUsersList;
+    private List<User> mMockUsersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-        mockUsersList = new ArrayList<User>();
-        //mockUsersList = MockDB.FRIENDS_LIST;
+        mMockUsersList = new ArrayList<User>();
         for (User user: MockDB.FRIENDS_LIST) {
-            mockUsersList.add(user);
+            mMockUsersList.add(user);
         }
-        mockUsersList.get(1).setOnline(true);
-        mockUsersList.get(3).setOnline(true);
-        //We sort the users to first display the online ones
-        Collections.sort(mockUsersList, new Comparator<User>(){
+        mMockUsersList.get(1).setOnline(true);
+        mMockUsersList.get(3).setOnline(true);
+        sortByOnline(mMockUsersList);
+        
+        // Create custom Adapter and pass it to the Activity
+        FriendListItemAdapter adapter = new FriendListItemAdapter(this, mMockUsersList);
+        setListAdapter(adapter);
+    }
+    
+    private void sortByOnline(List<User> userList) {
+        Collections.sort(userList, new Comparator<User>(){
 
             @Override
             public int compare(User user1, User user2) {
                 return Boolean.compare(user2.isOnline(), user1.isOnline());
             }
         });
-        
-        // Create custom Adapter and pass it to the Activity
-        FriendListItemAdapter adapter = new FriendListItemAdapter(this, mockUsersList);
-        setListAdapter(adapter);
     }
 
     @Override
