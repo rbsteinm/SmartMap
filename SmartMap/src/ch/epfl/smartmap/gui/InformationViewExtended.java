9b92d66d46f8427f0b321b2888d3536d5028ec1e
jfperren <julien.perrenoud@epfl.ch>
2014-11-18 00:02:32 +0100

    Fix merge bugs

--- SmartMap/src/ch/epfl/smartmap/gui/InformationViewExtended.java
+++ SmartMap/src/ch/epfl/smartmap/gui/InformationViewExtended.java
@@ -3,26 +3,10 @@
  */
 package ch.epfl.smartmap.gui;
 
-<<<<<<< HEAD
 import android.content.Context;
-import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import ch.epfl.smartmap.R;
-=======
-import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Display;
-import android.view.View;
-import android.view.Window;
-import android.widget.LinearLayout;
-import ch.epfl.smartmap.R;
-import ch.epfl.smartmap.activities.MainActivity;
->>>>>>> eeb2cf2d181fcb7ea3eed01dc505c27293ab6079
 import ch.epfl.smartmap.cache.Displayable;
 import ch.epfl.smartmap.cache.MockDB;
 
