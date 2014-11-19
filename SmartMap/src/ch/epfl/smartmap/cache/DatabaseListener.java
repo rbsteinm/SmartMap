fdb79ec9e3b91d54cd4a7c4c9ddb14b0932345dd
Nicolas Ritter <nicolas.ritter@epfl.ch>
2014-11-19 13:53:37 +0100

    Add DatabaseListener

---
+++ SmartMap/src/ch/epfl/smartmap/cache/DatabaseListener.java
@@ -0,0 +1,10 @@
+package ch.epfl.smartmap.cache;
+
+/**
+ * A listener for the DatabaseHelper class
+ * 
+ * @author ritterni
+ */
+public interface DatabaseListener {
+    void onNotified();
+}
--- SmartMap/src/ch/epfl/smartmap/cache/FriendList.java
+++ SmartMap/src/ch/epfl/smartmap/cache/FriendList.java
@@ -13,69 +13,69 @@ import android.util.LongSparseArray;
  */
 public class FriendList implements UserList {
 
-	private final List<Long> idList; // list of the friends' IDs
-	private String listName;
-	private long databaseID;
+    private final List<Long> idList; // list of the friends' IDs
+    private String listName;
+    private long databaseID;
 
-	/**
-	 * @param name
-	 *            The name of the friend list
-	 * @param friendsDatabase
-	 *            Whole database of friends referenced by the friendlist
-	 */
-	public FriendList(String name) {
-		// The friendlist needs a reference to the whole friend database in
-		// order to find users from their ID
-		listName = name;
-		idList = new ArrayList<Long>();
-	}
+    /**
+     * @param name
+     *            The name of the friend list
+     * @param friendsDatabase
+     *            Whole database of friends referenced by the friendlist
+     */
+    public FriendList(String name) {
+        // The friendlist needs a reference to the whole friend database in
+        // order to find users from their ID
+        listName = name;
+        idList = new ArrayList<Long>();
+    }
 
-	@Override
-	public String getListName() {
-		return listName;
-	}
+    @Override
+    public String getListName() {
+        return listName;
+    }
 
-	@Override
-	public void setListName(String newName) {
-		listName = newName;
-	}
+    @Override
+    public void setListName(String newName) {
+        listName = newName;
+    }
 
-	@Override
-	public void addUser(long id) {
-		if (!idList.contains(id)) {
-			idList.add(id);
-		}
-	}
+    @Override
+    public void addUser(long id) {
+        if (!idList.contains(id)) {
+            idList.add(id);
+        }
+    }
 
-	@Override
-	public void removeUser(long id) {
-		idList.remove(id);
-	}
+    @Override
+    public void removeUser(long id) {
+        idList.remove(id);
+    }
 
-	@Override
-	public List<Long> getList() {
-		return new ArrayList<Long>(idList);
-	}
+    @Override
+    public List<Long> getList() {
+        return new ArrayList<Long>(idList);
+    }
 
-	@Override
-	public List<User> getUserList(LongSparseArray<User> friends) {
-		List<User> uList = new ArrayList<User>();
-		for (long id : idList) {
-			uList.add(friends.get(id));
-		}
+    @Override
+    public List<User> getUserList(LongSparseArray<User> friends) {
+        List<User> uList = new ArrayList<User>();
+        for (long id : idList) {
+            uList.add(friends.get(id));
+        }
 
-		Collections.sort(uList, new UserComparator());
+        Collections.sort(uList, new UserComparator());
 
-		return uList;
-	}
+        return uList;
+    }
 
-	@Override
-	public long getID() {
-		return databaseID;
-	}
+    @Override
+    public long getID() {
+        return databaseID;
+    }
 
-	@Override
-	public void setID(long newID) {
-		databaseID = newID;
-	}
+    @Override
+    public void setID(long newID) {
+        databaseID = newID;
+    }
 }
