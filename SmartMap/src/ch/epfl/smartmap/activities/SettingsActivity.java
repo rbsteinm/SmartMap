92e9df4352a6cb98c706bc9dcba56980f7d56000
Robin Genolet <robin.genolet@epfl.ch>
2014-11-23 15:38:35 +0100

    Add author tag

--- SmartMap/src/ch/epfl/smartmap/activities/SettingsActivity.java
+++ SmartMap/src/ch/epfl/smartmap/activities/SettingsActivity.java
@@ -21,22 +21,16 @@ import ch.epfl.smartmap.cache.DatabaseHelper;
 import ch.epfl.smartmap.cache.SettingsManager;
 
 /**
- * A {@link PreferenceActivity} that presents a set of application settings. On handset devices, settings are
- * presented
- * as a single list. On tablets, settings are split by category, with category headers shown to the left of
- * the list of
+ * A {@link PreferenceActivity} that presents a set of application settings. On handset devices, settings are presented
+ * as a single list. On tablets, settings are split by category, with category headers shown to the left of the list of
  * settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html"> Android Design: Settings</a> for
- * design guidelines and the <a href="http://developer.android.com/guide/topics/ui/settings.html">Settings API
- * Guide</a> for more information on developing a Settings UI.
+ * 
+ * @author SpicyCH
  */
 public class SettingsActivity extends PreferenceActivity {
     /**
-     * Determines whether to always show the simplified settings UI, where settings are presented in a single
-     * list. When
-     * false, settings are shown as a master/detail two-pane view on tablets. When true, a single pane is
-     * shown on
+     * Determines whether to always show the simplified settings UI, where settings are presented in a single list. When
+     * false, settings are shown as a master/detail two-pane view on tablets. When true, a single pane is shown on
      * tablets.
      */
     private static final boolean ALWAYS_SIMPLE_PREFS = false;
@@ -63,7 +57,7 @@ public class SettingsActivity extends PreferenceActivity {
             this.getActionBar().setDisplayHomeAsUpEnabled(true);
             // Set action bar color to main color
             this.getActionBar().setBackgroundDrawable(
-                new ColorDrawable(this.getResources().getColor(R.color.main_blue)));
+                    new ColorDrawable(this.getResources().getColor(R.color.main_blue)));
         }
     }
 
@@ -85,8 +79,7 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     /**
-     * Shows the simplified settings UI if the device configuration if the device configuration dictates that
-     * a
+     * Shows the simplified settings UI if the device configuration if the device configuration dictates that a
      * simplified, single-pane UI should be shown.
      */
     @SuppressWarnings("deprecation")
@@ -128,8 +121,7 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     /**
-     * Helper method to determine if the device has an extra-large screen. For example, 10" tablets are
-     * extra-large.
+     * Helper method to determine if the device has an extra-large screen. For example, 10" tablets are extra-large.
      */
     private static boolean isXLargeTablet(Context context) {
         return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
@@ -137,15 +129,13 @@ public class SettingsActivity extends PreferenceActivity {
 
     /**
      * Determines whether the simplified settings UI should be shown. This is true if this is forced via
-     * {@link #ALWAYS_SIMPLE_PREFS}, or the device doesn't have newer APIs like {@link PreferenceFragment}, or
-     * the
-     * device doesn't have an extra-large screen. In these cases, a single-pane "simplified" settings UI
-     * should be
+     * {@link #ALWAYS_SIMPLE_PREFS}, or the device doesn't have newer APIs like {@link PreferenceFragment}, or the
+     * device doesn't have an extra-large screen. In these cases, a single-pane "simplified" settings UI should be
      * shown.
      */
     private static boolean isSimplePreferences(Context context) {
         return ALWAYS_SIMPLE_PREFS || (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
-            || !isXLargeTablet(context);
+                || !isXLargeTablet(context);
     }
 
     /** {@inheritDoc} */
@@ -160,35 +150,32 @@ public class SettingsActivity extends PreferenceActivity {
     /**
      * A preference value change listener that updates the preference's summary to reflect its new value.
      */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener =
-        new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object value) {
-                String stringValue = value.toString();
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
 
-                if (preference instanceof ListPreference) {
-                    // For list preferences, look up the correct display value in
-                    // the preference's 'entries' list.
-                    ListPreference listPreference = (ListPreference) preference;
-                    int index = listPreference.findIndexOfValue(stringValue);
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
 
-                    // Set the summary to reflect the new value.
-                    preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+                // Set the summary to reflect the new value.
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
 
-                } else {
-                    // For all other preferences, set the summary to the value's
-                    // simple string representation.
-                    preference.setSummary(stringValue);
-                }
-                return true;
+            } else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
             }
-        };
+            return true;
+        }
+    };
 
     /**
-     * Binds a preference's summary to its value. More specifically, when the preference's value is changed,
-     * its summary
-     * (line of text below the preference title) is updated to reflect the value. The summary is also
-     * immediately
+     * Binds a preference's summary to its value. More specifically, when the preference's value is changed, its summary
+     * (line of text below the preference title) is updated to reflect the value. The summary is also immediately
      * updated upon calling this method. The exact display format is dependent on the type of preference.
      * 
      * @see #sBindPreferenceSummaryToValueListener
@@ -200,12 +187,11 @@ public class SettingsActivity extends PreferenceActivity {
         // Trigger the listener immediately with the preference's
         // current value.
         sBindPreferenceSummaryToValueListener.onPreferenceChange(preference, PreferenceManager
-            .getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
+                .getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
     }
 
     /**
-     * This fragment shows general preferences only. It is used when the activity is showing a two-pane
-     * settings UI.
+     * This fragment shows general preferences only. It is used when the activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class GeneralPreferenceFragment extends PreferenceFragment {
@@ -220,8 +206,7 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     /**
-     * This fragment shows notification preferences only. It is used when the activity is showing a two-pane
-     * settings
+     * This fragment shows notification preferences only. It is used when the activity is showing a two-pane settings
      * UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -236,8 +221,7 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     /**
-     * This fragment shows data and sync preferences only. It is used when the activity is showing a two-pane
-     * settings
+     * This fragment shows data and sync preferences only. It is used when the activity is showing a two-pane settings
      * UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
