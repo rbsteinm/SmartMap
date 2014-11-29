3b187d4beea5d53d42bfe6c806d8465e64271def
Robin Genolet <robin.genolet@epfl.ch>
2014-11-29 01:56:30 +0100

    FriendPickerListAdapter now use the ViewHolder design pattern

--- SmartMap/src/ch/epfl/smartmap/gui/FriendPickerListAdapter.java
+++ SmartMap/src/ch/epfl/smartmap/gui/FriendPickerListAdapter.java
@@ -1,10 +1,8 @@
 package ch.epfl.smartmap.gui;
 
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,17 +13,17 @@ import ch.epfl.smartmap.R;
 import ch.epfl.smartmap.cache.User;
 
 /**
- * Displays each user in a row. The friend items are clickable to signify they are invited. Inspired by the adapter done
- * by Raphaël.
+ * Displays each user in a row. The friend items are clickable to signify they are invited. Use ViewHolder pattern as in
+ * {@link ch.epfl.smartmap.gui.EventsListItemAdapter}.
  * 
  * 
  * @author SpicyCH
  */
 public class FriendPickerListAdapter extends ArrayAdapter<User> {
 
+    @SuppressWarnings("unused")
     private static final String TAG = FriendPickerListAdapter.class.getSimpleName();
     private final Context mContext;
-    private final List<User> mItemsArrayList;
 
     /**
      * @param context
@@ -38,7 +36,6 @@ public class FriendPickerListAdapter extends ArrayAdapter<User> {
         super(context, R.layout.gui_friend_list_item, userList);
 
         mContext = context;
-        mItemsArrayList = userList;
     }
 
     /*
@@ -49,45 +46,31 @@ public class FriendPickerListAdapter extends ArrayAdapter<User> {
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO use ViewHolder pattern
 
-        // Create inflater,get item to construct
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.gui_friend_list_item, parent, false);
 
-        // Get FriendItem fields
-        TextView name = (TextView) convertView.findViewById(R.id.activity_friends_name);
-        TextView lastSeen = (TextView) convertView.findViewById(R.id.activity_friends_lastSeen);
-        ImageView picture = (ImageView) convertView.findViewById(R.id.activity_friends_picture);
+        ViewHolder viewHolder;
+        User user = this.getItem(position);
 
-        // Set the User's ID to the tag of its View
-        convertView.setTag(mItemsArrayList.get(position).getID());
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.gui_select_friend_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.name = (TextView) convertView.findViewById(R.id.activity_friends_name);
+            viewHolder.picture = (ImageView) convertView.findViewById(R.id.activity_friends_picture);
 
-        // Set fields with friend attributes
-        User user = mItemsArrayList.get(position);
-        name.setText(user.getName());
-        picture.setImageBitmap(mItemsArrayList.get(position).getPicture(mContext));
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
-        // build String "Last seen d/m/y at hour/min"
-        String lastSeenString = "Last seen " + user.getLastSeen().get(GregorianCalendar.DAY_OF_MONTH) + "/"
-                + (user.getLastSeen().get(GregorianCalendar.MONTH) + 1) + "/"
-                + user.getLastSeen().get(GregorianCalendar.YEAR) + " at "
-                + user.getLastSeen().get(GregorianCalendar.HOUR) + ":"
-                + user.getLastSeen().get(GregorianCalendar.MINUTE);
-
-        lastSeen.setText(lastSeenString);
-        lastSeen.setTextColor(Color.GRAY);
-
-        /*
-         * if ((mPositionsSelected.get(position) != null) && mPositionsSelected.get(position)) { // This item is
-         * selected convertView.setBackgroundColor(color.background_blue); } else {
-         * convertView.setBackgroundColor(color.main_grey); }
-         */
+        viewHolder.name.setText(user.getName());
+        viewHolder.picture.setImageBitmap(user.getPicture(mContext));
 
         return convertView;
     }
 
-    public void onItemClick() {
-
+    static class ViewHolder {
+        TextView name;
+        ImageView picture;
     }
 }
