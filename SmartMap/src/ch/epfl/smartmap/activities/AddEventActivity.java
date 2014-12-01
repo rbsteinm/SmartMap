66e6063f998f669927f32b3ded2da607c1e2dbe2
Alain Milliet <alain.milliet@epfl.ch>
2014-12-01 21:28:51 +0100

    Notification works now and addEvent too

--- SmartMap/src/ch/epfl/smartmap/activities/AddEventActivity.java
+++ SmartMap/src/ch/epfl/smartmap/activities/AddEventActivity.java
@@ -329,6 +329,8 @@ public class AddEventActivity extends FragmentActivity {
 		mDescription = (EditText) this.findViewById(R.id.addEventDescription);
 		mPlaceName = (EditText) this.findViewById(R.id.addEventPlaceName);
 
+		mPlaceName.setFocusable(true);
+
 		mTextChangedListener = new TextWatcher() {
 
 			@Override
@@ -432,11 +434,9 @@ public class AddEventActivity extends FragmentActivity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		cityName = addresses.get(0).getLocality();
-
-		if ((cityName != null) && !cityName.equals("")) {
+		if ((!addresses.isEmpty()) || ((cityName != null) && !cityName.equals(""))) {
+			cityName = addresses.get(0).getLocality();
 			mPlaceName.setText(cityName);
-			mPlaceName.setFocusable(true);
 			mGoogleMap.addMarker(new MarkerOptions().position(mEventPosition));
 			new DefaultZoomManager(mFragmentMap).zoomOnLocation(mEventPosition);
 		} else {
@@ -444,6 +444,8 @@ public class AddEventActivity extends FragmentActivity {
 			    Toast.LENGTH_LONG).show();
 			mPlaceName.setText("");
 		}
+		mGoogleMap.addMarker(new MarkerOptions().position(mEventPosition));
+		new DefaultZoomManager(mFragmentMap).zoomOnLocation(mEventPosition);
 	}
 
 	/**
