0edc21ea219881f4f95ff207f485d0f743064038
Hugo Sbai <hugo.sbai@epfl.ch>
2014-12-01 02:02:22 +0100

    Create interface and class to create a marker icon

---
+++ SmartMap/src/ch/epfl/smartmap/map/CircularMarkerIconMaker.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ch.epfl.smartmap.map;
+
+import android.graphics.Bitmap;
+
+/**
+ * An implementation of {@link MarkerIconMaker}, that creates circular icons for markers
+ * 
+ * @author hugo-S
+ */
+public class CircularMarkerIconMaker implements MarkerIconMaker {
+
+    /*
+     * (non-Javadoc)
+     * @see ch.epfl.smartmap.map.MarkerIconMaker#getMarkerIcon(android.graphics.Bitmap)
+     */
+    @Override
+    public Bitmap getMarkerIcon(Bitmap profilePicture) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
