98ac5f4d2b78f2ae706ce6a0ad78a67759d127c8
Nicolas Ritter <nicolas.ritter@epfl.ch>
2014-12-02 00:02:46 +0100

    Add updateFromCache method

--- SmartMap/src/ch/epfl/smartmap/database/DatabaseHelper.java
+++ SmartMap/src/ch/epfl/smartmap/database/DatabaseHelper.java
@@ -18,6 +18,7 @@ import android.graphics.BitmapFactory;
 import android.location.Location;
 import ch.epfl.smartmap.R;
 import ch.epfl.smartmap.background.SettingsManager;
+import ch.epfl.smartmap.cache.Cache;
 import ch.epfl.smartmap.cache.DefaultFilter;
 import ch.epfl.smartmap.cache.Displayable;
 import ch.epfl.smartmap.cache.Event;
@@ -934,6 +935,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
         return rows;
+    }
+
+    /**
+     * Updates the database contents to be up-to-date with the cache
+     */
+    public void updateFromCache() {
+        List<User> friends = Cache.getInstance().getAllFriends();
+        List<Event> events = Cache.getInstance().getAllGoingEvents();
+
+        mDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        mDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENT);
+        onCreate(mDatabase);
+
+        for (User user : friends) {
+            addUser(user.getImmutableCopy());
+        }
+        for (Event event : events) {
+            addEvent(event.getImmutableCopy());
+        }
     }
 
     /**
