12a3c2e45f8cb761b2f3aeab290cf0fb439bbed6
Robin Genolet <robin.genolet@epfl.ch>
2014-12-09 12:45:59 +0100

    More tests for Utils

--- SmartMapTest/src/ch/epfl/smartmaptest/utils/UtilsTest.java
+++ SmartMapTest/src/ch/epfl/smartmaptest/utils/UtilsTest.java
@@ -4,10 +4,12 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import android.location.Location;
 import android.test.AndroidTestCase;
 import ch.epfl.smartmap.R;
 import ch.epfl.smartmap.background.ServiceContainer;
 import ch.epfl.smartmap.background.SettingsManager;
+import ch.epfl.smartmap.cache.Displayable;
 import ch.epfl.smartmap.util.Utils;
 
 /**
@@ -20,6 +22,11 @@ public class UtilsTest extends AndroidTestCase {
     private Calendar tomorrow;
     private Calendar inTenDays;
     private Calendar tenDaysAgo;
+    private Calendar myBirthday;
+    private Calendar morning;
+    private Calendar afternoon;
+
+    private Location epfl;
 
     @Override
     protected void setUp() throws Exception {
@@ -37,12 +44,83 @@ public class UtilsTest extends AndroidTestCase {
         inTenDays = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));
         inTenDays.add(Calendar.DAY_OF_YEAR, 10);
 
-        tenDaysAgo = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));;
+        tenDaysAgo = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));
         tenDaysAgo.add(Calendar.DAY_OF_YEAR, -10);
+
+        myBirthday = new GregorianCalendar(1993, 9, 16);
+
+        morning = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));
+        morning.set(Calendar.HOUR_OF_DAY, 8);
+        morning.set(Calendar.MINUTE, 5);
+
+        afternoon = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));
+        afternoon.set(Calendar.HOUR_OF_DAY, 17);
+        afternoon.set(Calendar.MINUTE, 7);
+
+        epfl = new Location("");
+        epfl.setLatitude(46.519056);
+        epfl.setLongitude(6.566758);
     }
 
-    public void testInTenDays() {
+    public void testCitiyForLocation() {
+        assertEquals("Ecublens", Utils.getCityFromLocation(epfl));
+    }
 
+    public void testCountryForLocation() {
+        String result = Utils.getCountryFromLocation(epfl);
+
+        if ("Switzerland".equals(result) || "Suisse".equals(result)) {
+            // good
+        } else {
+            fail("EPFL wasn't in switzerland");
+        }
+    }
+
+    public void testGetDefaultValueForUnknownPlaces() {
+        Location somewhereVeryLost = new Location("");
+        somewhereVeryLost.setLatitude(0);
+        somewhereVeryLost.setLongitude(0);
+
+        assertEquals(Displayable.NO_LOCATION_STRING, Utils.getCityFromLocation(somewhereVeryLost));
+
+        assertEquals(Displayable.NO_LOCATION_STRING, Utils.getCountryFromLocation(somewhereVeryLost));
+
+    }
+
+    public void testLastSeenFiveMins() {
+        Calendar fiveMinsAgo = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT+01:00"));
+        fiveMinsAgo.add(Calendar.MINUTE, -5);
+
+        assertEquals(
+            "5 " + ServiceContainer.getSettingsManager().getContext().getString(R.string.utils_minutes_ago),
+            Utils.getLastSeenStringFromCalendar(fiveMinsAgo));
+    }
+
+    public void testLastSeenFiveYears() {
+
+    }
+
+    public void testLastSeenNow() {
+        assertEquals(ServiceContainer.getSettingsManager().getContext().getString(R.string.utils_now),
+            Utils.getLastSeenStringFromCalendar(now));
+    }
+
+    public void testLastSeenTenDays() {
+        assertEquals(
+            "10 " + ServiceContainer.getSettingsManager().getContext().getString(R.string.utils_days_ago),
+            Utils.getLastSeenStringFromCalendar(tenDaysAgo));
+    }
+
+    public void testMyBirthday() {
+        assertEquals("16.09.1993", Utils.getDateString(myBirthday));
+    }
+
+    public void testTimeAfternoon() {
+        assertEquals("17:07", Utils.getTimeString(afternoon));
+    }
+
+    public void testTimeMorning() {
+        assertEquals("08:05", Utils.getTimeString(morning));
     }
 
     public void testToday() {
