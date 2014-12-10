130444f2a442ef5355f0f969ef5763f4447b44cf
Matthieu Girod <matthieu.girod@epfl.ch>
2014-12-10 21:42:05 +0100

    Put back mContext in InvitationsTab to avoid crash...

--- SmartMap/src/ch/epfl/smartmap/gui/InvitationsTab.java
+++ SmartMap/src/ch/epfl/smartmap/gui/InvitationsTab.java
@@ -3,7 +3,9 @@ package ch.epfl.smartmap.gui;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -19,16 +21,17 @@ import ch.epfl.smartmap.callbacks.NetworkRequestCallback;
 import ch.epfl.smartmap.listeners.OnCacheListener;
 
 /**
- * TODO listen to invitation list?
- * Fragment diplaying your invitations in FriendsActivity
+ * Fragment displaying your invitations in FriendsActivity
  * 
  * @author marion-S
  */
 public class InvitationsTab extends ListFragment {
 
     private List<Invitation> mInvitationList;
+    private Context mContext;
 
-    public InvitationsTab() {
+    public InvitationsTab(Context ctx) {
+        mContext = ctx;
     }
 
     @Override
@@ -38,7 +41,7 @@ public class InvitationsTab extends ListFragment {
         mInvitationList = new ArrayList<Invitation>(ServiceContainer.getCache().getUnansweredFriendInvitations());
 
         // Create custom Adapter and pass it to the Activity
-        this.setListAdapter(new FriendInvitationListItemAdapter(this.getActivity(), mInvitationList));
+        this.setListAdapter(new FriendInvitationListItemAdapter(mContext, mInvitationList));
 
         // Initialize the listener
         ServiceContainer.getCache().addOnCacheListener(new OnCacheListener() {
@@ -46,11 +49,11 @@ public class InvitationsTab extends ListFragment {
             public void onInvitationListUpdate() {
                 mInvitationList =
                     new ArrayList<Invitation>(ServiceContainer.getCache().getUnansweredFriendInvitations());
-                InvitationsTab.this.getActivity().runOnUiThread(new Runnable() {
+                ((Activity) mContext).runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        InvitationsTab.this.setListAdapter(new FriendInvitationListItemAdapter(InvitationsTab.this
-                            .getActivity(), mInvitationList));
+                        InvitationsTab.this.setListAdapter(new FriendInvitationListItemAdapter(mContext,
+                            mInvitationList));
                     }
                 });
 
@@ -82,7 +85,7 @@ public class InvitationsTab extends ListFragment {
         super.onResume();
 
         mInvitationList = new ArrayList<Invitation>(ServiceContainer.getCache().getUnansweredFriendInvitations());
-        this.setListAdapter(new FriendInvitationListItemAdapter(this.getActivity(), mInvitationList));
+        this.setListAdapter(new FriendInvitationListItemAdapter(mContext, mInvitationList));
     }
 
     /*
@@ -101,38 +104,36 @@ public class InvitationsTab extends ListFragment {
      *            the user's id
      */
     private void displayAcceptFriendDialog(final Invitation invitation) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
-        builder.setMessage(this.getActivity().getString(R.string.add) + " " + invitation.getUser().getName() + " "
-            + this.getActivity().getString(R.string.as_a_friend));
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setMessage(mContext.getString(R.string.add) + " " + invitation.getUser().getName() + " "
+            + mContext.getString(R.string.as_a_friend));
 
         // Add positive button
-        builder.setPositiveButton(this.getActivity().getString(R.string.yes_accept),
-            new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int id) {
-                    // Accept invitation in Cache
-                    ServiceContainer.getCache().acceptInvitation(invitation, new NetworkRequestCallback() {
-                        @Override
-                        public void onFailure() {
-                            InvitationsTab.this.getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(
-                                        InvitationsTab.this.getActivity(),
-                                        InvitationsTab.this.getActivity().getString(
-                                            R.string.accept_friend_network_error), Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
+        builder.setPositiveButton(mContext.getString(R.string.yes_accept), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // Accept invitation in Cache
+                ServiceContainer.getCache().acceptInvitation(invitation, new NetworkRequestCallback() {
+                    @Override
+                    public void onFailure() {
+                        ((Activity) mContext).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(mContext,
+                                    ((Activity) mContext).getString(R.string.accept_friend_network_error),
+                                    Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
 
-                        @Override
-                        public void onSuccess() {
-                            // We show the user's informations
-                            InvitationsTab.this.startActivity(invitation.getIntent());
-                        }
-                    });
-                }
-            });
+                    @Override
+                    public void onSuccess() {
+                        // We show the user's informations
+                        InvitationsTab.this.startActivity(invitation.getIntent());
+                    }
+                });
+            }
+        });
 
         // Add negative button
         builder.setNegativeButton(R.string.no_decline, new DialogInterface.OnClickListener() {
@@ -142,11 +143,11 @@ public class InvitationsTab extends ListFragment {
                 ServiceContainer.getCache().declineInvitation(invitation, new NetworkRequestCallback() {
                     @Override
                     public void onFailure() {
-                        InvitationsTab.this.getActivity().runOnUiThread(new Runnable() {
+                        ((Activity) mContext).runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(InvitationsTab.this.getActivity(),
-                                    InvitationsTab.this.getActivity().getString(R.string.decline_friend_network_error),
+                                Toast.makeText(mContext,
+                                    ((Activity) mContext).getString(R.string.decline_friend_network_error),
                                     Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -154,11 +155,10 @@ public class InvitationsTab extends ListFragment {
 
                     @Override
                     public void onSuccess() {
-                        InvitationsTab.this.getActivity().runOnUiThread(new Runnable() {
+                        ((Activity) mContext).runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(InvitationsTab.this.getActivity(),
-                                    InvitationsTab.this.getActivity().getString(R.string.decline_confirm),
+                                Toast.makeText(mContext, mContext.getString(R.string.decline_confirm),
                                     Toast.LENGTH_SHORT).show();
                             }
                         });
