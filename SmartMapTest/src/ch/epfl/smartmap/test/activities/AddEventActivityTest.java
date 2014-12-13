215eadbe6a20606318ec543d7c2497ebf7a93bc6
Robin Genolet <robin.genolet@epfl.ch>
2014-12-13 18:55:02 +0100

    Tests pass for AddEvent

--- SmartMapTest/src/ch/epfl/smartmap/test/activities/AddEventActivityTest.java
+++ SmartMapTest/src/ch/epfl/smartmap/test/activities/AddEventActivityTest.java
@@ -28,6 +28,9 @@ import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 public class AddEventActivityTest extends ActivityInstrumentationTestCase2<AddEventActivity> {
 
     private static final String TEST_NAME = "This is a test event";
+    private static final String TEST_NAME_HAS_DESCR = "This test event has a description";
+
+    private static final int ESPRESSO_WAIT_DELAY = 1000;
 
     public AddEventActivityTest() {
         super(AddEventActivity.class);
@@ -52,17 +55,52 @@ public class AddEventActivityTest extends ActivityInstrumentationTestCase2<AddEv
 
         String name = "SmartMap SwEng";
 
-        // TODO this line throw IllegalArgException
+        ServiceContainer.getNetworkClient().authServer(name, facebookId, token);
+
         ServiceContainer.getCache().putUser(
             new UserContainer(1, name, "123", "abc@abc.com", loc, "Mock lockation", User.NO_IMAGE,
                 User.BlockStatus.UNBLOCKED, User.SELF));
 
-        ServiceContainer.getNetworkClient().authServer(name, facebookId, token);
-
-        Thread.sleep(1000);
+        Thread.sleep(ESPRESSO_WAIT_DELAY);
 
         ServiceContainer.getSettingsManager().setLocation(loc);
 
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        this.getActivity();
+        super.tearDown();
+    }
+
+    public void testCanCreateEventWithDescription() throws InterruptedException {
+        onView(withId(R.id.addEventEventName)).perform(ViewActions.typeText(TEST_NAME));
+
+        Espresso.pressBack();
+        Thread.sleep(ESPRESSO_WAIT_DELAY);
+
+        onView(withId(R.id.addEventDescription)).perform(ViewActions.typeText(TEST_NAME_HAS_DESCR));
+
+        Espresso.pressBack();
+        Thread.sleep(ESPRESSO_WAIT_DELAY);
+
+        onView(withId(R.id.addEventButtonCreateEvent)).perform(ViewActions.click());
+
+        this.getActivity();
+
+        onView(withId(R.id.show_event_info_invite_friends_button)).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    public void testCanCreateEventWithoutDescription() {
+        onView(withId(R.id.addEventEventName)).perform(ViewActions.typeText(TEST_NAME));
+
+        onView(withId(R.id.addEventButtonCreateEvent)).perform(ViewActions.click());
+
+        this.getActivity();
+
+        onView(withId(R.id.show_event_info_invite_friends_button)).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
     public void testCannnotCreateEventWithoutPlaceName() {
@@ -97,7 +135,7 @@ public class AddEventActivityTest extends ActivityInstrumentationTestCase2<AddEv
         Espresso.pressBack();
 
         // Espresso doesn't wait the end of the pressback.
-        Thread.sleep(1000);
+        Thread.sleep(ESPRESSO_WAIT_DELAY);
 
         onView(withId(R.id.addEventButtonCreateEvent)).perform(ViewActions.click());
         onView(withId(R.id.addEventDescription)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
@@ -105,18 +143,7 @@ public class AddEventActivityTest extends ActivityInstrumentationTestCase2<AddEv
 
     public void testOpenSetLocationWhenClickOnMap() throws InterruptedException {
         onView(withId(R.id.add_event_map)).perform(click());
-        Thread.sleep(1000);
+        Thread.sleep(ESPRESSO_WAIT_DELAY);
         onView(withId(R.id.set_location_activity)).check(matches(isDisplayed()));
-    }
-
-    public void testZZZCanCreateEventWithoutDescription() throws InterruptedException {
-        onView(withId(R.id.addEventEventName)).perform(ViewActions.typeText(TEST_NAME));
-
-        onView(withId(R.id.addEventButtonCreateEvent)).perform(ViewActions.click());
-
-        this.getActivity();
-
-        onView(withId(R.id.show_event_info_invite_friends_button)).check(
-            ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
