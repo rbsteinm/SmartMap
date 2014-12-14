9bc81389bf5b64282941cefdfce8fa1c9f3fe32b
Robin Genolet <robin.genolet@epfl.ch>
2014-12-14 15:00:46 +0100

    Tests for ShowEvents pass

--- SmartMapTest/src/ch/epfl/smartmap/test/activities/ShowEventsActivityTest.java
+++ SmartMapTest/src/ch/epfl/smartmap/test/activities/ShowEventsActivityTest.java
@@ -10,15 +10,29 @@ import static org.hamcrest.Matchers.not;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TimeZone;
 
+import org.mockito.Mockito;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.SeekBar;
 import ch.epfl.smartmap.R;
 import ch.epfl.smartmap.activities.ShowEventsActivity;
+import ch.epfl.smartmap.background.ServiceContainer;
+import ch.epfl.smartmap.cache.Cache;
+import ch.epfl.smartmap.cache.Event;
+import ch.epfl.smartmap.cache.EventContainer;
+import ch.epfl.smartmap.cache.PublicEvent;
+import ch.epfl.smartmap.cache.Self;
+import ch.epfl.smartmap.cache.User;
+import ch.epfl.smartmap.cache.UserContainer;
+import ch.epfl.smartmap.servercom.SmartMapClientException;
 import ch.epfl.smartmap.util.Utils;
 
 import com.google.android.apps.common.testing.ui.espresso.UiController;
@@ -31,7 +45,7 @@ import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
  * Test different features of ShowEventActivity. I might need to rethink the
  * tests if the server interferes with them
  * later.
- * 
+ *
  * @author SpicyCH
  */
 public class ShowEventsActivityTest extends ActivityInstrumentationTestCase2<ShowEventsActivity> {
@@ -50,66 +64,46 @@ public class ShowEventsActivityTest extends ActivityInstrumentationTestCase2<Sho
         mActivity = this.getActivity();
 
         this.addMockEventsInDB();
-    }
-
-    @Override
-    protected void tearDown() {
-        this.getActivity().finish();
-    }
-
-    public void testCanClickOnAnEvent() throws InterruptedException {
-
-        // Clicks the first element of the list
-        onView(withId(0)).perform(ViewActions.click());
-        // Espresso.onData(anything()).inAdapterView(withContentDescription("listview")).atPosition(0)
-        // .perform(ViewActions.click());
-
-        // The dialog may need a network access that might take some time to be
-        // displayed
-        Thread.sleep(2000);
-
-        // If the AlertDialog has been opened, then the button2 must be
-        // displayed. Espresso sometimes cannot find this view, but relaunch the
-        // test if this happens, or make espresso functional.
-        onView(ViewMatchers.withId(android.R.id.button3)).check(
-            ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
     }
 
     public void testCanOpenAddEventActivity() {
-        openActionBarOverflowOrOptionsMenu(this.getInstrumentation().getTargetContext());
+        Context context = this.getInstrumentation().getTargetContext();
+        openActionBarOverflowOrOptionsMenu(context);
 
-        onView(withText("Create a new event")).perform(ViewActions.click());
+        onView(withText(context.getString(R.string.showEventsMenuNewEvent))).perform(ViewActions.click());
 
         onView(withId(R.id.addEventDescription)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
     }
 
     public void testCheckingMyEventsChangesListSize() throws InterruptedException {
-        Thread.sleep(2000);
-        int initialSize = mActivity.getListView().getCount();
+
+        mActivity = this.getActivity();
+
+        int initialSize = mActivity.getListAdapter().getCount();
+
         onView(withId(R.id.ShowEventsCheckBoxMyEv)).perform(ViewActions.click());
         mActivity = this.getActivity();
-        int finalSize = mActivity.getListView().getCount();
+        int finalSize = mActivity.getListAdapter().getCount();
         assertTrue("finalSize: " + finalSize + " wasn't smaller than initialSize: " + initialSize,
             finalSize < initialSize);
     }
 
     public void testCheckingNearMeChangesListSize() {
-        // FIXME this test fails sometimes for NO REASONS, wtf???
-        int initialSize = mActivity.getListView().getCount();
+        int initialSize = mActivity.getListAdapter().getCount();
         onView(withId(R.id.ShowEventsCheckBoxNearMe)).perform(ViewActions.click());
         mActivity = this.getActivity();
-        int finalSize = mActivity.getListView().getCount();
+        int finalSize = mActivity.getListAdapter().getCount();
         assertTrue("finalSize: " + finalSize + " wasn't smaller than initialSize: " + initialSize,
             finalSize < initialSize);
     }
 
     public void testCheckingOnGoingChangesListSize() {
-        int initialSize = mActivity.getListView().getCount();
+        int initialSize = mActivity.getListAdapter().getCount();
         onView(withId(R.id.ShowEventscheckBoxStatus)).perform(ViewActions.click());
         mActivity = this.getActivity();
-        int finalSize = mActivity.getListView().getCount();
+        int finalSize = mActivity.getListAdapter().getCount();
         assertTrue("finalSize: " + finalSize + " wasn't smaller than initialSize: " + initialSize,
             finalSize < initialSize);
     }
@@ -127,17 +121,17 @@ public class ShowEventsActivityTest extends ActivityInstrumentationTestCase2<Sho
     }
 
     public void testSeekBarChangesListSize() {
-        int initialSize = mActivity.getListView().getCount();
+        int initialSize = mActivity.getListAdapter().getCount();
         onView(withId(R.id.ShowEventsCheckBoxNearMe)).perform(ViewActions.click());
         onView(withId(R.id.showEventSeekBar)).perform(setProgress(0));
         mActivity = this.getActivity();
-        int finalSize = mActivity.getListView().getCount();
+        int finalSize = mActivity.getListAdapter().getCount();
         assertTrue("finalSize: " + finalSize + " wasn't smaller than initialSize: " + initialSize,
             finalSize < initialSize);
 
         onView(withId(R.id.showEventSeekBar)).perform(setProgress(100));
         mActivity = this.getActivity();
-        int newFinalSize = mActivity.getListView().getCount();
+        int newFinalSize = mActivity.getListAdapter().getCount();
         assertTrue("finalSize: " + finalSize + " wasn't smaller than newFinalSize: " + initialSize,
             finalSize < newFinalSize);
     }
@@ -146,90 +140,103 @@ public class ShowEventsActivityTest extends ActivityInstrumentationTestCase2<Sho
         onView(withId(R.id.showEventSeekBar)).check(matches(not(isEnabled())));
     }
 
-    private void addMockEventsInDB() {
-        /*
-         * GregorianCalendar timeE0 = new GregorianCalendar();
-         * timeE0.add(GregorianCalendar.MINUTE, -5);
-         * GregorianCalendar timeEndE0 = new GregorianCalendar();
-         * timeEndE0.add(GregorianCalendar.HOUR_OF_DAY, 5);
-         * Location lutry = new Location("Lutry");
-         * lutry.setLatitude(46.506038);
-         * lutry.setLongitude(6.685314);
-         * PublicEvent e0 = new PublicEvent("Now Event", 2, "Robich", timeE0,
-         * timeEndE0, lutry);
-         * e0.setID(0);
-         * e0.setPositionName("Lutry");
-         * GregorianCalendar timeE1 = new GregorianCalendar();
-         * timeE1.add(GregorianCalendar.DAY_OF_YEAR, 5);
-         * GregorianCalendar timeEndE1 = new GregorianCalendar();
-         * timeEndE1.add(GregorianCalendar.DAY_OF_YEAR, 10);
-         * Location lausanne = new Location("Lausanne");
-         * lausanne.setLatitude(46.519962);
-         * lausanne.setLongitude(6.633597);
-         * PublicEvent e1 = new PublicEvent("Swag party", 2, "Robich", timeE1,
-         * timeEndE1, lausanne);
-         * e1.setID(1);
-         * e1.setPositionName("Lausanne");
-         * GregorianCalendar timeE2 = new GregorianCalendar();
-         * timeE2.add(GregorianCalendar.HOUR_OF_DAY, 3);
-         * GregorianCalendar timeEndE2 = new GregorianCalendar();
-         * timeEndE2.add(GregorianCalendar.DAY_OF_YEAR, 2);
-         * Location epfl = new Location("EPFL");
-         * epfl.setLatitude(46.526120);
-         * epfl.setLongitude(6.563778);
-         * PublicEvent e2 = new PublicEvent("LOL Tournament", 1, "Alain",
-         * timeE2, timeEndE2, epfl);
-         * e2.setID(2);
-         * e2.setPositionName("EPFL");
-         * GregorianCalendar timeE3 = new GregorianCalendar();
-         * timeE3.add(GregorianCalendar.HOUR_OF_DAY, 1);
-         * GregorianCalendar timeEndE3 = new GregorianCalendar();
-         * timeEndE3.add(GregorianCalendar.HOUR, 5);
-         * Location verbier = new Location("Verbier");
-         * verbier.setLatitude(46.096076);
-         * verbier.setLongitude(7.228875);
-         * PublicEvent e3 = new PublicEvent("Freeride World Tour", 1, "Julien",
-         * timeE3, timeEndE3, verbier);
-         * e3.setID(3);
-         * e3.setPositionName("Verbier");
-         * String descrE3 =
-         * "It�s a vertical free-verse poem on the mountain. It�s the ultimate expression of all that"
-         * + "is fun and liberating about sliding on snow in wintertime.";
-         * e3.setDescription(descrE3);
-         * DatabaseHelper dbHelper = DatabaseHelper.getInstance();
-         * dbHelper.clearAll();
-         * dbHelper.addEvent(e0);
-         * dbHelper.addEvent(e1);
-         * dbHelper.addEvent(e2);
-         * dbHelper.addEvent(e3);
-         */
+    public void testZCanClickOnRefresh() {
+        Context context = this.getInstrumentation().getTargetContext();
+        openActionBarOverflowOrOptionsMenu(context);
+
+        onView(withText(context.getString(R.string.show_events_menu_refresh))).perform(ViewActions.click());
+    }
+
+    private void addMockEventsInDB() throws SmartMapClientException, InterruptedException {
+
+        ServiceContainer.initSmartMapServices(this.getActivity());
+
+        Location loc = new Location("");
+        loc.setLatitude(46.519056);
+        loc.setLongitude(6.566758);
+
+        String token =
+            "CAAEWMqbRPIkBAJjvxMI0zNXLgzxYJURV5frWkDu8T60EfWup92GNEE7xDIVohfpa43Qm7FNbZCvZB7bXVTd0ZC0qLHZCju2zZBR3mc8mQH0OskEe7X5mZAWOlLZCIzsAWnfEy1ZAzz2JgYPKjaIwhIpI9OvJkQNWkJnX3rIwv4v9lL7hr9yx8LKuOegEHfZCcCNp491jewilZCz69ZA2ohryEYy";
+
+        long facebookId = 1482245642055847L;
+
+        String name = "SmartMap SwEng";
+
+        UserContainer userContainer =
+            new UserContainer(1, name, "123", "abc@abc.com", loc, "Mock lockation", User.NO_IMAGE,
+                User.BlockStatus.UNBLOCKED, User.SELF);
+
+        ServiceContainer.getCache().putUser(userContainer);
+
+        Thread.sleep(1000);
+
+        ServiceContainer.getSettingsManager().setLocation(loc);
 
         Calendar now = GregorianCalendar.getInstance(TimeZone.getTimeZone(Utils.GMT_SWITZERLAND));
-        Calendar inTenMins = GregorianCalendar.getInstance(TimeZone.getTimeZone(Utils.GMT_SWITZERLAND));
-        inTenMins.add(Calendar.MINUTE, 10);
+        Calendar inFiveMins = GregorianCalendar.getInstance(TimeZone.getTimeZone(Utils.GMT_SWITZERLAND));
+        inFiveMins.add(Calendar.MINUTE, 5);
 
-        Location location = new Location("");
-        location.setLatitude(20);
-        location.setLongitude(20);
+        EventContainer eventContainer =
+            new EventContainer(1, "Test event", userContainer, "test descr", now, inFiveMins, loc, "EPFL",
+                new HashSet<Long>());
 
-        /*
-         * ImmutableEvent event =
-         * new ImmutableEvent(1, "Test event", 2, "Description.......", now,
-         * inTenMins, location,
-         * "Somewhere", new HashSet<Long>());
-         * ServiceContainer.getCache().createEvent(event, new
-         * NetworkRequestCallback() {
-         * @Override
-         * public void onFailure() {
-         * // Nothing
-         * }
-         * @Override
-         * public void onSuccess() {
-         * // Nothing
-         * }
-         * });
-         */
+        User me = new Self();
+        Set<Long> participants = new HashSet<Long>();
 
+        GregorianCalendar timeE0 = new GregorianCalendar();
+        timeE0.add(GregorianCalendar.MINUTE, -5);
+        GregorianCalendar timeEndE0 = new GregorianCalendar();
+        timeEndE0.add(GregorianCalendar.HOUR_OF_DAY, 5);
+        Location lutry = new Location("Lutry");
+        lutry.setLatitude(46.506038);
+        lutry.setLongitude(6.685314);
+        PublicEvent e0 =
+            new PublicEvent(1, "Lutry fun", me, timeE0, timeEndE0, lutry, "Lutry", "Le fun à lutry",
+                participants);
+
+        GregorianCalendar timeE1 = new GregorianCalendar();
+        timeE1.add(GregorianCalendar.DAY_OF_YEAR, 5);
+        GregorianCalendar timeEndE1 = new GregorianCalendar();
+        timeEndE1.add(GregorianCalendar.DAY_OF_YEAR, 10);
+        Location lausanne = new Location("Lausanne");
+        lausanne.setLatitude(46.519962);
+        lausanne.setLongitude(6.633597);
+        PublicEvent e1 =
+            new PublicEvent(2, "Electrosanne", me, timeE1, timeEndE1, lausanne, "Lausanne", "boom boom",
+                participants);
+
+        GregorianCalendar timeE2 = new GregorianCalendar();
+        timeE2.add(GregorianCalendar.HOUR_OF_DAY, 3);
+        GregorianCalendar timeEndE2 = new GregorianCalendar();
+        timeEndE2.add(GregorianCalendar.DAY_OF_YEAR, 2);
+        Location epfl = new Location("EPFL");
+        epfl.setLatitude(46.526120);
+        epfl.setLongitude(6.563778);
+        PublicEvent e2 =
+            new PublicEvent(3, "Espresso tesing", me, timeE2, timeEndE2, epfl, "EPFL",
+                "have a cup of Espresso", participants);
+
+        GregorianCalendar timeE3 = new GregorianCalendar();
+        timeE3.add(GregorianCalendar.HOUR_OF_DAY, 1);
+        GregorianCalendar timeEndE3 = new GregorianCalendar();
+        timeEndE3.add(GregorianCalendar.HOUR, 5);
+        Location verbier = new Location("Verbier");
+        verbier.setLatitude(1);
+        verbier.setLongitude(2);
+        PublicEvent e3 =
+            new PublicEvent(4, "Far away", me, timeE3, timeEndE3, verbier, "Somewhere very far",
+                "hello", participants);
+
+        Set<Event> allEvents = new HashSet<Event>();
+        allEvents.add(e0);
+        allEvents.add(e1);
+        allEvents.add(e2);
+        allEvents.add(e3);
+
+        Cache cache = Mockito.mock(Cache.class);
+        Mockito.when(cache.getAllEvents()).thenReturn(allEvents);
+
+        ServiceContainer.setCache(cache);
     }
 
     // from stackoverflow
